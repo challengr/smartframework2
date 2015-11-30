e3d8161e2995e5eedbbbe5501d442e0e96f83c8b
Wangzhe <wangz2357@gmail.com>
2015-11-30 11:13:23 +0800

    This initial part of the chapter2

---
+++ src/main/java/org.smart4j.chapter2/controller/CustomerEditServlet.java
@@ -0,0 +1,7 @@
+package org.smart4j.chapter2.controller;
+
+/**
+ * Created by Wangzhe on 2015/11/30.
+ */
+public class CustomerEditServlet {
+}
---
+++ src/main/java/org.smart4j.chapter2/model/Consumer.java
@@ -0,0 +1,88 @@
+package org.smart4j.chapter2.model;
+
+/**
+ * 客户类
+ * Created by Wangzhe on 2015/11/30.
+ */
+public class Consumer {
+    private long id;
+
+    private String name;
+
+    private String contact;
+
+    private String telephone;
+
+    private String email;
+
+    private String remark;
+
+
+    // Getters and setters
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public long getId() {
+
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    // To String
+
+    @Override
+    public String toString() {
+        return "Consumer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", contact='" + contact + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", email='" + email + '\'' +
+                ", remark='" + remark + '\'' +
+                '}';
+    }
+}
